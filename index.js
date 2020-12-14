f8718ba4468620c461091554833034a6b314c1cd
Andres J <Andres.F.Jimenez.F@gmail.com>
2020-12-13 19:46:19 -0800

    set the calls for the database and some the response for what the user chooses

--- index.js
+++ index.js
@@ -41,24 +41,22 @@ const promptUser = () => {
     ]).then(answer => {
         if (answer.action === "Add Employee") {
             ///Should add info to sq file
-            //addName()
-
+             return addName();
         } else if (answer.action === "View All Employees") {
             //view employes by department but with inquirer option to bring by department
-            writeAll();
+            return writeAll();
         } else if (answer.action === "View All Employees by Department") {
             //VIEW EMPLOYEES BY DEPARTMENT
-
+            return departmentsByEmployees();
         } else if (answer.action === "View All Roles") {
             //VIEW ALL ROLES
-            rolesAll();
-
+            return rolesAll();
         } else if (answer.action === "Add Department") {
             //ADD A DEPARTMENT
-
+            return AddNewDepartment();
         } else if (answer.action === "Remove Department") {
             //REMOVE DEPARTMENT
-
+            return deleteDepartment()
         }else if (answer.action === "View All Employess by Manager") {
             //VIEW ALL EMPLOYEES BY MANAGER
         }
@@ -75,10 +73,46 @@ const promptUser = () => {
 
 }
 
-function addName(data) {
-    console.log(data)
-    addEmployee(data)
-    promptUser()
+async function addName() {
+    const roles  = await Call.findallRoles();
+    const employees = await Call.allEmployees();
+    const promptName = await inquirer.prompt([
+        {
+            name:"first_name",
+            message:"Employee first name?",
+        },
+        {
+            name:"last_name",
+            message:"Employee last name?"
+        }
+    ]);
+    const roleChoices = roles.map(({id, position})=>({
+        name: position,
+        value: id
+    }));
+    const {roleId} = await inquirer.prompt({
+        type:"list",
+        name:"roleId",
+        message:"Whast the employee position?",
+        choices: roleChoices
+    });
+    promptName.roles_id = roleId;
+
+    const managerChoice = employees.map(({id, first_name, last_name})=>({
+        name:`${first_name} ${last_name}`,
+        value: id 
+    }));
+    managerChoice.unshift({name:"N/A",value:null});
+    const {managerId} = await inquirer.prompt({
+        type: "list",
+        name: "managerId",
+        message: "Whos the employee manager?",
+        choices: managerChoice
+    })
+    promptName.manager_id = managerId;
+    await Call.addEmployees(promptName)
+    console.log(`"${promptName.first_name} ${promptName.last_name}" was added to your employee list.`)
+    promptUser();
 }
 
 async function writeAll(){
@@ -94,7 +128,56 @@ async function rolesAll(){
     console.table(listRoles)
     promptUser()
 }
+async function departmentsByEmployees(){
+    const allDepartments = await Call.findAllDepeartmets();
+    const choices = allDepartments.map(({id,name})=>({
+        name:name,
+        value:id
+    }));
+    const {departmentChoice} = await inquirer.prompt([
+        {
+            type:"list",
+            name:"departmentChoice",
+            message:"Which departments employee's will you like to see?",
+            choices:choices
+        }
+    ])
+    const listOfEmployees = await Call.findAllEmployeesByDepartments(departmentChoice);
+    console.log("\n");
+    console.table(listOfEmployees);
+    promptUser();
+}
 
+async function AddNewDepartment(){
+    const newDepartment = await inquirer.prompt([
+        {
+            name:"name",
+            message:"What's the name of the department you've created?"
+        }
+    ])
+    await Call.createDepartment(newDepartment);
+    
+    console.log(`Added a new department: ${newDepartment.name}`)
+    promptUser()
+}
+async function deleteDepartment(){
+    const department = await Call.findAllDepeartmets()
+    const departmentChoices = department.map(({id,name})=>({
+        name:name,
+        value:id
+    }))
+    const departmentChoosen = await inquirer.prompt([
+        {
+            type:"list",
+            name:"choice",
+            message:"Which department will you like to delete?",
+            choices:departmentChoices
+        }
+    ])
+    await Call.removeDepartment(departmentChoosen)
+    console.log(`${departmentChoosen.choice} was removed from your Department list.`)
 
+    promptUser();
+}
 
 start();
